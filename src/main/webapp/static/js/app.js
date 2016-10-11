e7c3e690d6f3592d47434f0bcb6b71a3c3c5e9cf
netphenix <netphenix@netphenix>
2016-10-11 11:23:52 +0530

    Merge sprint1-Arun into sprint1-Arul
    Conflicts:
    	src/main/webapp/index.html

--- src/main/webapp/static/js/app.js
+++ src/main/webapp/static/js/app.js
@@ -1,2 +1,1 @@
-//var app = angular.module("VizBoardApp", ['angularScreenfull', 'ngDraggable']);
-var app = angular.module("VizBoardApp", ["ui.router","ngTable"]);
+var app = angular.module("VizBoardApp", ["ui.router", "angularScreenfull", "ngDraggable"]);
