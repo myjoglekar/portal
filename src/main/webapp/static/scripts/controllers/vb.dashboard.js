6fe78033b2e82be6afc45e79eb52ac3009236acb
Firstname Lastname <your.email@example.org>
2016-10-13 11:36:46 +0530

    changed index.html

--- src/main/webapp/static/scripts/controllers/vb.dashboard.js
+++ src/main/webapp/static/scripts/controllers/vb.dashboard.js
@@ -10,42 +10,42 @@ app.controller("DashboardController", function ($scope, $http) {
 
 
 
-    //Start Line Chart
-
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
-
-    var formatDate = d3.time.format("%d-%b-%y");
-
-    var x = d3.time.scale()
-            .range([0, width]);
-
-    var y = d3.scale.linear()
-            .range([height, 0]);
-    var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left");
-
-    var line = d3.svg.line()
-            .x(function (d) {
-                return x(d.date);
-            })
-            .y(function (d) {
-                return y(d.close);
-            });
-
-    var svg = d3.select("#lineChart").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-    //End Line Chart
+//    //Start Line Chart
+//
+//    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+//    width = 960 - margin.left - margin.right,
+//            height = 500 - margin.top - margin.bottom;
+//
+//    var formatDate = d3.time.format("%d-%b-%y");
+//
+//    var x = d3.time.scale()
+//            .range([0, width]);
+//
+//    var y = d3.scale.linear()
+//            .range([height, 0]);
+//    var xAxis = d3.svg.axis()
+//            .scale(x)
+//            .orient("bottom");
+//
+//    var yAxis = d3.svg.axis()
+//            .scale(y)
+//            .orient("left");
+//
+//    var line = d3.svg.line()
+//            .x(function (d) {
+//                return x(d.date);
+//            })
+//            .y(function (d) {
+//                return y(d.close);
+//            });
+//
+//    var svg = d3.select("#lineChart").append("svg")
+//            .attr("width", width + margin.left + margin.right)
+//            .attr("height", height + margin.top + margin.bottom)
+//            .append("g")
+//            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//    
+//    //End Line Chart
     
     
     $http.get('static/datas/labels.json').success(function (response) {
@@ -60,43 +60,44 @@ app.controller("DashboardController", function ($scope, $http) {
             console.log(value);
             
             if (value.type === 'line') {
-                d3.tsv("static/datas/lineChart.tsv", type, function (error, data) {
-                    if (error)
-                        throw error;
-
-                    x.domain(d3.extent(data, function (d) {
-                        return d.date;
-                    }));
-                    y.domain(d3.extent(data, function (d) {
-                        return d.close;
-                    }));
-
-                    svg.append("g")
-                            .attr("class", "x axis")
-                            .attr("transform", "translate(0," + height + ")")
-                            .call(xAxis);
-
-                    svg.append("g")
-                            .attr("class", "y axis")
-                            .call(yAxis)
-                            .append("text")
-                            .attr("transform", "rotate(-90)")
-                            .attr("y", 6)
-                            .attr("dy", ".71em")
-                            .style("text-anchor", "end")
-                            .text("Price ($)");
-
-                    svg.append("path")
-                            .datum(data)
-                            .attr("class", "line")
-                            .attr("d", line);
-                });
-                var formatDate = d3.time.format("%d-%b-%y");
-                function type(d) {
-                    d.date = formatDate.parse(d.date);
-                    d.close = +d.close;
-                    return d;
-                }
+               // $("#div1").load("static/views/charts/lineChart.html");
+//                d3.tsv("static/datas/lineChart.tsv", type, function (error, data) {
+//                    if (error)
+//                        throw error;
+//
+//                    x.domain(d3.extent(data, function (d) {
+//                        return d.date;
+//                    }));
+//                    y.domain(d3.extent(data, function (d) {
+//                        return d.close;
+//                    }));
+//
+//                    svg.append("g")
+//                            .attr("class", "x axis")
+//                            .attr("transform", "translate(0," + height + ")")
+//                            .call(xAxis);
+//
+//                    svg.append("g")
+//                            .attr("class", "y axis")
+//                            .call(yAxis)
+//                            .append("text")
+//                            .attr("transform", "rotate(-90)")
+//                            .attr("y", 6)
+//                            .attr("dy", ".71em")
+//                            .style("text-anchor", "end")
+//                            .text("Price ($)");
+//
+//                    svg.append("path")
+//                            .datum(data)
+//                            .attr("class", "line")
+//                            .attr("d", line);
+//                });
+//                var formatDate = d3.time.format("%d-%b-%y");
+//                function type(d) {
+//                    d.date = formatDate.parse(d.date);
+//                    d.close = +d.close;
+//                    return d;
+//                }
             }
 
         });
@@ -141,20 +142,20 @@ app.controller("DashboardController", function ($scope, $http) {
     $http.get('static/datas/defaultPanel.json').success(function (response) {
         $scope.panels = response;
         angular.forEach(response, function (value, key) {
-            $http.get(value.url).success(function (response) {
-                dataPoints = response;
-                var chart = new CanvasJS.Chart("canvasContainers" + value.chartId, {
-                    theme: "theme2", //theme1
-                    animationEnabled: false, // change to true
-                    data: [
-                        {
-                            type: value.chartType,
-                            dataPoints: response
-                        }
-                    ]
-                });
-                chart.render();
-            });
+//            $http.get(value.url).success(function (response) {
+//                dataPoints = response;
+//                var chart = new CanvasJS.Chart("canvasContainers" + value.chartId, {
+//                    theme: "theme2", //theme1
+//                    animationEnabled: false, // change to true
+//                    data: [
+//                        {
+//                            type: value.chartType,
+//                            dataPoints: response
+//                        }
+//                    ]
+//                });
+//                chart.render();
+//            });
         });
     });
 
