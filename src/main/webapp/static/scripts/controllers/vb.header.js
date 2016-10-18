5c81ea47df90ab7502138c88b5bd5b46bc10b3a4
netphenix <netphenix@netphenix>
2016-10-18 15:08:42 +0530

    changed Bootstrap

--- src/main/webapp/static/scripts/controllers/vb.header.js
+++ src/main/webapp/static/scripts/controllers/vb.header.js
@@ -1,8 +1,12 @@
-app.controller('HeaderController', function($scope, $http, $stateParams){
-    $http.get('static/datas/dashboard.json').success(function(response){
+app.controller('HeaderController', function ($scope, $http, $stateParams) {
+
+    $http.get('static/datas/dashboard.json').success(function (response) {
         $scope.dashboards = response;
-    })
+    });
+
+    $scope.selectName = "Select Dashboard";
+    $scope.change = function (dashboard) {
+        $scope.selectName = dashboard.dashboardName;
+    };
     
-    $stateParams.type = $scope.tabType;
-    console.log("Header : "+$stateParams.type)
 })
