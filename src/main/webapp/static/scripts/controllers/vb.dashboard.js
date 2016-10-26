f06cd6ac495dc83ae25bc5eb6dcdfa6261d78972
Firstname Lastname <your.email@example.org>
2016-10-26 14:30:53 +0530

    panels.html

--- src/main/webapp/static/scripts/controllers/vb.dashboard.js
+++ src/main/webapp/static/scripts/controllers/vb.dashboard.js
@@ -1,16 +1,4 @@
 app.controller("DashboardController", function ($scope, $http, $stateParams) {
-    //Add Panels 
-     
-
-    //Default Load Tab   
-
-//    $http.get("static/datas/" + $stateParams.tabId + ".json").success(function (response) {
-//        $scope.panels = response;
-//    });
-
-
-    
-
     $http.get('static/datas/labels.json').success(function (response) {
         $scope.labels = response;
     });
@@ -19,7 +7,7 @@ app.controller("DashboardController", function ($scope, $http, $stateParams) {
         $scope.chartTypes = response;
     });
 
-    var dataPoints = [];
+    
     var value = [];
     $scope.previewChart = function (chartType, panel) {
         panel.selectedChartType = chartType;
@@ -29,9 +17,7 @@ app.controller("DashboardController", function ($scope, $http, $stateParams) {
         value.type = chartType.type;
         value.chartId = panel.chartId;
         console.log("Values : " + value.title + " " + value.url + " " + value.type);
-//        $http.get(panel.url).success(function (response) {
-//            dataPoints = response;           
-
+       
         if (value.type === "line") {
             lineChart(value);
         } else if (value.type === "area") {
@@ -50,21 +36,38 @@ app.controller("DashboardController", function ($scope, $http, $stateParams) {
             alert("No Charts Available");
         }
 
-        //console.log("dataPoints : "+dataPoints)
-        //});
     };
+    
+//    $scope.save = function (chartType,panel) {
+//        panel.selectedChartType = chartType;
+//        console.log("save:"+panel.title + " " + panel.url + " " + chartType.type + " " + panel.chartId);
+//        value.title = panel.title;
+//        value.url = panel.url;
+//        value.type = chartType.type;
+//        value.chartId = panel.chartId;
+//        console.log("Values : " + value.title + " " + value.url + " " + value.type);
+//       
+//        if (value.type === "line") {
+//            lineChart(value);
+//        } else if (value.type === "area") {
+//            areaChart(value);
+//        } else if (value.type === "bar") {
+//            barChart(value);
+//        } else if (value.type === "groupedbar") {
+//            groupedBarChart(value);
+//        } else if (value.type === "pie") {
+//            pieChart(value);
+//        } else if (value.type === "donut") {
+//            donutChart(value);
+//        } else if (value.type === "table") {
+//            table(value);
+//        }else {
+//            alert("No Charts Available");
+//        }
+//
+//    };
 
-    //Default Load Chart
-//    $http.get('static/datas/defaultPanel.json').success(function (response) {
-//        $scope.panels = response;
-//        angular.forEach(response, function (value, key) {
-////            if (value.chartType == "line") {
-////                lineChart(value);
-////            } else if (value.chartType == "area") {
-//            // areaChart(value)
-////            }
-//        });
-//    });
+    
 
     $scope.charts = [];
     $http.get('static/datas/d3chart.json').success(function (response, error) {
