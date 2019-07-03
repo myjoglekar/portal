c1de0c1daf5d7c61a490f95bc0c39f83ff8a91b9
myjoglekar <myjo@hotmail.com>
2019-07-03 09:45:17 -0500

    Update DealerVaultService.java

--- api/src/main/java/com/visumbu/api/admin/service/DealerVaultService.java
+++ api/src/main/java/com/visumbu/api/admin/service/DealerVaultService.java
@@ -28,7 +28,7 @@ import org.json.simple.parser.ParseException;
 
 /**
  *
- * @author jp
+ * @author Mandar Y. Joglekar
  */
 @Service("dealerVaultService")
 @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
